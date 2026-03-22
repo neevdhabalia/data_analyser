a77c6dcfa06c4cc38e824c3ac0f22a8100c8d8d8
neevdhabalia <neevdhabalia@gmail.com>
2026-03-22 19:27:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title("📊 Dataset Analyzer App")
+
+uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+
+    st.write("### Dataset Preview")
+    st.dataframe(df)
+
+    st.write("### Column Selection")
+    column = st.selectbox("Choose a column", df.columns)
+
+ #adding colums for histogram
+   
+    if df[column].dtype != 'object':
+    	fig, ax = plt.subplots(figsize=(8,5))
+    	ax.hist(df[column], bins=10, edgecolor='black')
+    	ax.set_title(f"Distribution of {column}")
+    	ax.set_xlabel(column)
+    	ax.set_ylabel("Frequency")
+    	st.pyplot(fig)
+    else:
+    	st.write("This column is not numeric, cannot plot histogram.")
+    
+#adding correlation and covariance
+    
+
+    st.write("### Correlation & Covariance")
+
+    col1 = st.selectbox("Select first column", df.columns)
+    col2 = st.selectbox("Select second column", df.columns)
+
+# Only for numeric data
+    if df[col1].dtype != 'object' and df[col2].dtype != 'object':
+    
+    	correlation = df[col1].corr(df[col2])
+    	covariance = df[col1].cov(df[col2])
+
+    	st.write(f"Correlation between {col1} and {col2}: ", correlation)
+    	st.write(f"Covariance between {col1} and {col2}: ", covariance)
+
+    else:
+    	st.write("Please select numeric columns only.")
+    
